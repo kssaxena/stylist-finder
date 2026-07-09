20f8ca9bf0227e5960858a82a73547f90ce131a9
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-09 22:43:35 +0530

    feat: add store and storeStaff models with comprehensive schemas for managing store and staff details

---
+++ backend/src/models/store.model.js
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+
+const storeSchema = new mongoose.Schema(
+  {
+    storeName: { type: String },
+    storeContactNumber: { type: String },
+    storeEmail: { type: String },
+    address: { type: mongoose.Schema.Types.ObjectId, ref: "Address" },
+    bank: { type: mongoose.Schema.Types.ObjectId, ref: "BankDetails" },
+    storeStaffs: { type: mongoose.Schema.Types.ObjectId, ref: "StoreStaff" },
+    services: { type: mongoose.Schema.Types.ObjectId, ref: "Services" },
+    bookings: { type: mongoose.Schema.Types.ObjectId, ref: "Bookings" },
+
+    // enums
+    serviceType: {
+      type: String,
+      enum: ["In House", "On Site", "Both (In house and On site)"],
+      default: "Both",
+    },
+    paymentOptions: {
+      inStore: { type: String, enum: ["Online (Cards / UPI)", "Cash", "Both"] },
+      onSite: { type: String, enum: ["UPI", "Cash", "Both"] },
+    },
+
+    // extras for profile
+    storeTimings: { openFrom: { type: Date }, openTill: { type: Date } },
+    images: {
+      logo: { url: String, fileId: String },
+      gallery: [{ url: String, fileId: String }],
+    },
+
+    // owner details
+    owner: {
+      ownerName: { type: String, required: true },
+      ownerContact: { type: String, required: true },
+      ownerEmail: { type: String, required: true },
+      ownerAddress: { type: mongoose.Schema.Types.ObjectId, ref: "Address" },
+      aadhar: {
+        number: { type: Number },
+        image: {
+          front: { url: String, fileId: String },
+          back: { url: String, fileId: String },
+        },
+      },
+      pan: {
+        number: { type: Number },
+        image: { url: String, fileId: String },
+      },
+    },
+
+    // store kyc
+    pan: {
+      number: { type: Number },
+      image: { url: String, fileId: String },
+    },
+    gst: {
+      number: { type: Number },
+      image: { url: String, fileId: String },
+    },
+
+    // authentications
+    otp: { type: String, default: null },
+    otpExpiry: { type: Date, default: null },
+    role: "Store",
+
+    // admin controls
+    isActive: { type: Boolean, default: true },
+    isVerified: { type: Boolean, default: false },
+    ownerKycComplete: { type: Boolean, default: false },
+    storeKycComplete: { type: Boolean, default: false },
+    isSubscribed: { type: Boolean, default: true },
+    subscription: { type: mongoose.Schema.Types.ObjectId, ref: "Subscription" },
+  },
+  { timestamps: true },
+);
+
+export const Store = mongoose.model("Store", storeSchema);
---
+++ backend/src/models/storeStaff.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const storeStaffSchema = new mongoose.Schema(
+  {
+    store: { type: mongoose.Schema.Types.ObjectId, ref: "Store" },
+
+    name: { type: String, required: true },
+    contactNumber: { type: String, required: true },
+    email: { type: String, required: true },
+    specialization: String,
+    otherServices: [String],
+    address: { type: mongoose.Schema.Types.ObjectId, ref: "Address" },
+    profileImage: { url: String, fileId: String },
+    designation: String,
+    experience: String,
+
+    // kyc
+    aadhar: {
+      number: { type: Number },
+      image: {
+        front: { url: String, fileId: String },
+        back: { url: String, fileId: String },
+      },
+    },
+    pan: {
+      number: { type: Number },
+      image: { url: String, fileId: String },
+    },
+
+    // store controls
+    isActive: { type: Boolean, default: true },
+    isVerified: { type: Boolean, default: false },
+    kycComplete: { type: Boolean, default: false },
+  },
+  { timestamps: true },
+);
+
+export const StoreStaff = mongoose.Schema("StoreStaff", storeStaffSchema);
