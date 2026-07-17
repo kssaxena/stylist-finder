9827141162b66dc08ae5d71f155a7b6bd1f2c8fc
Akanksha Kumari <157986893+Akanksha4127@users.noreply.github.com>
2026-07-17 18:12:01 +0530

    feat: Implement FetchData utility for API requests with axios

---
+++ frontend/src/utils/FetchFromApi.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export const FetchData = async (url, method, data, file = false) => {
+  const Base_URL = `${process.env.DomainUrl}/api/v1`;
+  const AccessToken = localStorage.getItem("AccessToken");
+
+  const options = {
+    headers: {
+      "Content-Type": file ? "multipart/form-data" : "application/json",
+      Authorization: `Bearer ${AccessToken}`,
+    },
+    withCredentials: true,
+  };
+
+  if (method === "get") {
+    const response = await axios.get(`${Base_URL}/${url}`, {
+      params: data,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${AccessToken}`,
+      },
+    });
+    return response;
+  } else if (method === "post") {
+    const response = await axios.post(`${Base_URL}/${url}`, data, options);
+    return response;
+  } else if (method === "delete") {
+    const response = await axios.delete(`${Base_URL}/${url}`, options);
+    return response;
+  } else {
+    return "Please enter the valid method";
+  }
+};
