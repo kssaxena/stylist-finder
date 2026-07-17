89a21d596f61a0f10caed65191b63166c977c03d
Akanksha Kumari <157986893+Akanksha4127@users.noreply.github.com>
2026-07-17 18:11:46 +0530

    feat: Add customer registration input constants for form handling

---
+++ frontend/src/constants/constants.js
@@ -0,0 +1,20 @@
+export const customerRegistrationInputs = [
+  {
+    label: "Name",
+    placeholder: "Enter your name",
+    name: "name",
+    type: "text",
+  },
+  {
+    label: "Contact Number",
+    placeholder: "Enter your contact number",
+    name: "contactNumber",
+    type: "text",
+  },
+  {
+    label: "Email",
+    placeholder: "Enter your email",
+    name: "email",
+    type: "email",
+  },
+];
