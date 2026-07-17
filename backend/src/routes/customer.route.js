409b8dce699bf018bef1e6ed94e55741203ed22e
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-17 15:58:07 +0530

    feat: add customer middleware for token verification and secure routes

---
+++ backend/src/routes/customer.route.js
@@ -0,0 +1,53 @@
+import { Router } from "express";
+import {
+  addAlternateContactNumber,
+  otpVerification,
+  registerCustomer,
+  updateGender,
+  updateAlternateNumber,
+  addAddress,
+  markAddressDefault,
+  updateAddress,
+  deleteAddress,
+  addBankDetails,
+  addUPIid,
+  dashboardData,
+} from "../controllers/customer.controller.js";
+
+import { VerifyCustomer } from "../middlewares/customer.middleware.js";
+
+const router = Router();
+
+router.route("/register").post(registerCustomer);
+router.route("/login").post(registerCustomer);
+router
+  .route("/otp/authentication/:verificationType/:customerId")
+  .post(otpVerification);
+router.route("/update/gender/:customerId").post(VerifyCustomer, updateGender);
+router
+  .route("/update/add/alternate-contact-number/:customerId")
+  .post(VerifyCustomer, addAlternateContactNumber);
+router
+  .route("/update/modify/alternate-contact-number/:customerId")
+  .post(VerifyCustomer, updateAlternateNumber);
+router
+  .route("/update/add-address/:customerId")
+  .post(VerifyCustomer, addAddress);
+router
+  .route("/update/add-address/:customerId/:addressId")
+  .post(VerifyCustomer, markAddressDefault);
+router
+  .route("/update/modify-address/:customerId/:addressId")
+  .post(VerifyCustomer, updateAddress);
+router
+  .route("/update/delete-address/:customerId/:addressId")
+  .post(VerifyCustomer, deleteAddress);
+router
+  .route("/update/add-bank-details/:customerId")
+  .post(VerifyCustomer, addBankDetails);
+router
+  .route("/update/add-upi-details/:customerId")
+  .post(VerifyCustomer, addUPIid);
+router
+  .route("/get/dashboard/data/:customerId/:query")
+  .get(VerifyCustomer, dashboardData);
