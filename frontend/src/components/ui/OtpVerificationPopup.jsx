6d59e7cef18040ef6d1e85474304d7c677a65447
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-21 14:46:07 +0530

    feat: enhance OtpVerificationPopup for dynamic userType handling and improved OTP verification flow

--- frontend/src/components/ui/OtpVerificationPopup.jsx
+++ frontend/src/components/ui/OtpVerificationPopup.jsx
@@ -2,34 +2,50 @@ import React, { useRef, useState } from "react";
 import Popup from "./Popup"; // Update path if needed
 import Button from "../Button";
 import InputBox from "../Input";
+import { FetchData } from "../../utils/FetchFromApi";
+import { useToast } from "../hooks/ToastContext";
+import { useDispatch } from "react-redux";
+import { addUser, clearUser } from "../../redux/slice/authSlice";
 
 function OtpVerificationPopup({
   isOpen,
   onClose,
   data,
   verificationType = "",
+  userType,
 }) {
   const [otp, setOtp] = useState("");
   const formRef = useRef();
+  const { alertSuccess, alertError, alertInfo } = useToast();
+  const dispatch = useDispatch();
+  console.log(data);
 
   const verifyOtp = async (e) => {
     e.preventDefault();
+    console.log("UI worked");
     try {
       const formData = new FormData(formRef.current);
       const response = await FetchData(
-        `customer/otp/authentication/${verificationType}/${data?.user?._id}`,
+        `${userType}/otp/authentication/${verificationType}/${data?.user?._id}`,
         "post",
         formData,
       );
-      if (response.status === 200) {
-        alert("Success", "OTP Verified");
+      console.log(response);
+      const { user, tokens } = response.data.data;
 
-        setOtpPopup(false);
+      localStorage.setItem("AccessToken", tokens.accessToken);
+      localStorage.setItem("RefreshToken", tokens.refreshToken);
+      localStorage.setItem("role", user.role);
 
-        navigate("/dashboard");
-      }
-    } catch (error) {
-      alert("Error", "Invalid OTP");
+      dispatch(clearUser());
+      dispatch(addUser(user));
+      // openModelOTP(false);
+      formRef.current.reset();
+      onClose();
+      alertSuccess(response.data.message || "Otp Verified successfully !");
+    } catch (err) {
+      console.log(err);
+      alertError(err?.response?.data);
     }
   };
 
@@ -62,10 +78,15 @@ function OtpVerificationPopup({
             maxLength={6}
             onChange={(e) => setOtp(e.target.value.replace(/\D/g, ""))}
           />
+          <input
+            name="contactNumber"
+            value={data?.user?.contactNumber}
+            className="hidden"
+          />
         </div>
 
         <div className="w-full">
-          <Button LabelName="Verify OTP" onClick={verifyOtp} />
+          <Button LabelName="Verify OTP" type="submit" />
         </div>
       </form>
     </Popup>
