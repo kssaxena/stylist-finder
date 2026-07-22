de0dc5c797fba91596214f98612ed97861d73dfb
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-22 17:34:49 +0530

    feat: enhance constants with new icons and add DashboardSectionList for improved dashboard functionality

--- frontend/src/constants/Constants.jsx
+++ frontend/src/constants/Constants.jsx
@@ -1,5 +1,16 @@
-import { FaAward } from "react-icons/fa";
+import { BsBank2 } from "react-icons/bs";
+import {
+  FaAward,
+  FaHeart,
+  FaImages,
+  FaMapMarkerAlt,
+  FaServicestack,
+  FaUser,
+} from "react-icons/fa";
+import { GoVerified } from "react-icons/go";
 import { IoIosHappy } from "react-icons/io";
+import { IoStorefrontSharp } from "react-icons/io5";
+import { MdDomainVerification } from "react-icons/md";
 import { RiFlowerFill, RiTeamFill } from "react-icons/ri";
 
 export const ChooseUs = [
@@ -22,5 +33,68 @@ export const ChooseUs = [
     icon: <IoIosHappy />,
     name: "Relax And Rejuvanate",
     description: "Wellness therapies for mind & body ",
+  },
+];
+
+export const DashboardSectionList = [
+  {
+    label: "Overview",
+    icon: <GoVerified />,
+    query: "overview",
+    roles: ["Customer", "Store", "Professional"],
+  },
+  {
+    label: "Address",
+    icon: <FaMapMarkerAlt />,
+    query: "address",
+    roles: ["Customer", "Store", "Professional"],
+  },
+  {
+    label: "Bank Details",
+    icon: <BsBank2 />,
+    query: "bankDetails",
+    roles: ["Customer", "Store", "Professional"],
+  },
+  {
+    label: "Favorite Store",
+    icon: <IoStorefrontSharp />,
+    query: "fav_store",
+    roles: ["Customer"],
+  },
+  {
+    label: "Favorite Professional",
+    icon: <FaHeart />,
+    query: "fav_professional",
+    roles: ["Customer"],
+  },
+  {
+    label: "Wishlist Services",
+    icon: <FaServicestack />,
+    query: "wishlist_services",
+    roles: ["Customer"],
+  },
+  {
+    label: "Store Staff",
+    icon: <FaUser />,
+    query: "store_staff",
+    roles: ["Store"],
+  },
+  {
+    label: "Own Services ",
+    icon: <FaUser />,
+    query: "own_services",
+    roles: ["Store", "Professional"],
+  },
+  {
+    label: "Images",
+    icon: <FaImages />,
+    query: "images",
+    roles: ["Store", "Professional"],
+  },
+  {
+    label: "KYC",
+    icon: <MdDomainVerification />,
+    query: "kyc",
+    roles: ["Store", "Professional"],
   },
 ];
